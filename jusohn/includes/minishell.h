a8862b4eb26f6921e2e7b97bedceb2c07b7d6d76
Jun Young Sohn <jusohn@student.42seoul.kr>
2023-06-22 15:19:00 +0900

    [jusohn] MISC: initial commit for code from jusohn with readline lib and minishell pdf

---
+++ jusohn/includes/minishell.h
@@ -0,0 +1,136 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jusohn <jusohn@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/14 16:18:24 by jusohn            #+#    #+#             */
+/*   Updated: 2023/06/17 13:57:47 by jusohn           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# ifndef DEBUG
+#	define DEBUG						1
+# endif
+
+/* ================ INCLUDES ================= */
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <errno.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+# include "readline/readline.h"
+# include "readline/history.h"
+
+/* ================== TYPES ================== */
+typedef int							t_bool;
+typedef unsigned long long			t_size;
+typedef long long					t_ssize;
+
+typedef enum	e_token_type
+{
+	TOKEN_UNKNOWN,
+	TOKEN_PIPE,
+	TOKEN_AND,
+	TOKEN_OR,
+	TOKEN_WILDCARD,
+	TOKEN_L_PARENTHESIS,
+	TOKEN_R_PARNTHESIS,
+	TOKEN_REDIR,
+	TOKEN_REDIR_IN,
+	TOKEN_REDIR_OUT,
+	TOKEN_HEREDOC,
+	TOKEN_REDIR_APPEND,
+	TOKEN_S_QUOTE,
+	TOKEN_D_QUOTE,
+	TOKEN_DOLLAR_SIGN,
+	TOKEN_ENV_VAR,
+	TOKEN_WORD,
+	TOKEN_WHITESPACE,
+	TOKEN_EOL
+}	t_token_type;
+
+typedef struct  s_token
+{
+  t_token_type	type;
+  char			*value;
+}   t_token;
+
+typedef enum e_ast_node_type
+{
+    AST_UNKNOWN,
+    AST_COMMAND,
+    AST_ARGUMENT,
+    AST_PIPE,
+    AST_REDIR_IN,
+    AST_REDIR_OUT,
+    AST_REDIR_APPEND,
+	AST_HEREDOC,
+	AST_SUBSHELL
+} t_ast_node_type;
+
+typedef struct  s_ast_node
+{
+	t_ast_node_type		type;
+	char				**cmd_args;
+	t_size				num_args;
+	struct s_ast_node	*left;
+	struct s_ast_node	*right;
+}   t_ast_node;
+
+/* ================== MACRO ================== */
+# define STD_IN						0
+# define STD_OUT					1
+# define STD_ERR					2
+
+# define READ						0
+# define WRITE						1
+
+# define EXIT_NORMAL				0
+# define EXIT_ERR					1
+# define EXIT_NOT_EXECUTABLE		126
+# define EXIT_CMD_NOT_FOUND			127
+
+# define FT_TRUE					1
+# define FT_FALSE					0
+
+/* ================== MEMORY_UTILS ================== */
+void		ft_bzero(void *s, t_size n);
+void		*ft_calloc(t_size count, t_size size);
+void		*ft_memset(void *b, int c, t_size len);
+void		*ft_memcpy(void *dst, const void *src, t_size n);
+
+/* ================== STR_UTILS ================== */
+int			ft_strcmp(const char *s1, const char *s2);
+int			ft_strncmp(const char *s1, const char *s2, t_size n);
+t_size		ft_strlen(const char *str);
+size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize);
+int			ft_isalnum(int c);
+
+/* ================ STR_UTILS_CREATE ================ */
+char		*ft_strtrim(char *str, char c);
+char		*ft_strdup(char *src);
+
+/* ================== TOKENIZER ================== */
+t_token 	*tokenize(const char *input, t_size *num_tokens);
+void 		print_tokens(t_token *tokens, t_size num_tokens);
+void 		categorize_tokens(t_token *tokens, t_size num_tokens);
+
+/* ================== LEXER ================== */
+t_token		*create_token(t_token_type type, const char *buffer, int buffer_length);
+
+/* ================== PARSER ================== */
+t_ast_node	*parse_tokens(t_token *tokens, t_size num_tokens);
+void		free_ast(t_ast_node *root);
+
+/* ================== EXECUTOR ================== */
+int			execute_ast(t_ast_node *root);
+
+# endif
---
+++ jusohn/memory/mem_utils.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mem_utils.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jusohn <jusohn@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/15 13:55:09 by jusohn            #+#    #+#             */
+/*   Updated: 2023/06/15 13:55:52 by jusohn           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	ft_bzero(void *s, t_size n)
+{
+	unsigned char	*tmp;
+
+	tmp = (unsigned char *) s;
+	while (0 < n--)
+		*(tmp++) = 0;
+}
+
+void	*ft_calloc(t_size count, t_size size)
+{
+	void	*ptr;
+
+	ptr = malloc (size * count);
+	if (!ptr)
+		return (NULL);
+	ft_bzero(ptr, size * count);
+	return (ptr);
+}
+
+void	*ft_memset(void *b, int c, t_size len)
+{
+	unsigned char	*tmp;
+
+	tmp = (unsigned char *) b;
+	while (0 < len--)
+		*(tmp++) = (unsigned char) c;
+	return (b);
+}
+
+void	*ft_memcpy(void *dst, const void *src, t_size n)
+{
+	unsigned char	*tmp_dst;
+	unsigned char	*tmp_src;
+
+	tmp_src = (unsigned char *) src;
+	tmp_dst = (unsigned char *) dst;
+	if (!src && !dst)
+		return (dst);
+	while (0 < n--)
+		*(tmp_dst++) = *(tmp_src++);
+	return (dst);
+}
