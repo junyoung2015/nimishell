d1d04c17ba82a407afa6c599591752385591c9b0
nijesmik <bkybb@duck.com>
2023-07-21 22:57:15 +0900

    [BUILTIN] BUG : echo 옵션 처리 잘 되게 수정 #10

--- builtin/echo.c
+++ builtin/echo.c
@@ -1,17 +1,38 @@
 #include "builtin.h"
 
+/*
+
+할일
+2. env 모두 복사
+3. exit | ls 안됨
+4. echo | cat 안됨
+
+*/
+
+t_bool	is_option(char *str)
+{
+	if (!str)
+		return (FALSE);
+	if (*str++ != '-')
+		return (FALSE);
+	if (!*str)
+		return (FALSE);
+	while (*str == 'n')
+		str++;
+	if (*str)
+		return (FALSE);
+	return (TRUE);
+}
+
 void	echo(t_node *node)
 {
 	t_bool	option_n;
 	size_t	i;
 
-	option_n = FALSE;
+	option_n = is_option(node->cmd_args[1]);
 	i = 1;
-	if (node->cmd_args[1] && node->cmd_args[1][0] == '-' && node->cmd_args[1][1] == 'n')
-	{
-		option_n = TRUE;
+	if (option_n)
 		i++;
-	}
 	while (node->cmd_args[i])
 	{
 		write(STDOUT_FILENO, node->cmd_args[i], ft_strlen(node->cmd_args[i]));
