d148990e95e9830e9dc6dbf3906c8636a68cfbc5
jusohn <jusohn@student.42seoul.kr>
2023-08-05 16:22:26 +0900

    [WILD] FIX: wildcard expansion 결과 정렬 시 나오는 SEGV on unknown address 수정 #32

--- substitution/ft_qsort.c
+++ substitution/ft_qsort.c
@@ -1,13 +1,8 @@
 #include "minishell.h"
 
-int	cmp_ascii(const void *a, const void *b)
+int	cmp_ascii(void *a, void *b)
 {
-	return ft_strcmp((const char *)a, (const char *)*b);
-}
-
-int	cmp_len(const void *a, const void *b)
-{
-	return ft_strlen(*(char **)a) - ft_strlen(*(char **)b);
+	return ft_strcmp((const char *)a, (const char *)b);
 }
 
 void	ft_swap(void **a, void **b)
@@ -19,16 +14,16 @@ void	ft_swap(void **a, void **b)
 	*b = tmp;
 }
 
-int	ft_partition(void **arr, t_ssize low, t_ssize high, int (*cmp)(const void *, const void *))
+int	ft_partition(void **arr, t_ssize l, t_ssize h, int (*cmp)(void *, void *))
 {
 	void	*pivot;
 	t_ssize	i;
 	t_ssize	j;
 
-	pivot = arr[high];
-	i = low - 1;
-	j = low;
-	while (j < high)
+	pivot = arr[h];
+	i = l - 1;
+	j = l;
+	while (j < h)
 	{
 		if (cmp(arr[j], pivot) < 0)
 		{
@@ -37,19 +32,19 @@ int	ft_partition(void **arr, t_ssize low, t_ssize high, int (*cmp)(const void *,
 		}
 		j++;
 	}
-	ft_swap((void **) &arr[i + 1], &(arr[high]));
+	ft_swap(&arr[i + 1], &arr[h]);
 	return (i + 1);
 }
 
-void	ft_qsort(void **arr, t_ssize low, t_ssize high, int (*cmp)(const void *, const void *))
+void	ft_qsort(void **arr, t_ssize l, t_ssize h, int (*cmp)(void *, void *))
 {
 	t_ssize	pivot;
 
-	if (low < high)
+	if (l < h)
 	{
-		pivot = ft_partition(arr, low, high, cmp);
-		ft_qsort(arr, low, pivot - 1, cmp);
-		ft_qsort(arr, pivot + 1, high, cmp);
+		pivot = ft_partition(arr, l, h, cmp);
+		ft_qsort(arr, l, pivot - 1, cmp);
+		ft_qsort(arr, pivot + 1, h, cmp);
 	}
 }
 
