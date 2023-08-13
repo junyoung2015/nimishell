c2484bf16c7b5036162021571450ed6154a13d9d
jusohn <jusohn@student.42seoul.kr>
2023-08-13 17:53:23 +0900

    [PARSE] FIX: && || 연산자가 올시 연산자가 부모 노드,  명령어가 자식 노드에 오도록 수정 #40

--- parser/parser_ll.c
+++ parser/parser_ll.c
@@ -870,37 +870,33 @@ t_node *parse_list_tail(t_parser *parser, t_node *parent)
 	(void)			parent;
 
 	logic_node = 0;
+	state = AST_AND;
+	if (check(parser, TOKEN_OR))
+		state = AST_OR;
+	logic_node = create_node(state);
+	if (!logic_node)	// malloc err
+		return (0);
+	advance(parser);
+	pipeline_node = parse_pipeline(parser, logic_node);
+	if (!pipeline_node)
+	{
+		// free and handle err here?
+		free(logic_node);
+		return (0);
+	}
+	if (pipeline_node->type == AST_ERR)
+		return (logic_node);
 	if (check(parser, TOKEN_AND) || check(parser, TOKEN_OR))
 	{
-		state = AST_AND;
-		if (check(parser, TOKEN_OR))
-			state = AST_OR;
-		logic_node = create_node(state);
-		if (!logic_node)	// malloc err
+		list_tail_node = parse_list_tail(parser, pipeline_node);
+		if (!list_tail_node) // err?
 			return (0);
-		// logic_node->left = parent;
-		// append_child_node(logic_node, parent);
-		advance(parser);
-		pipeline_node = parse_pipeline(parser, logic_node);
-		if (!pipeline_node)
-		{
-			// free and handle err here?
-			free(logic_node);
-			return (0);
-		}
+		logic_node->right = list_tail_node;
+		list_tail_node->left = pipeline_node;
+	}
+	else
+	{
 		logic_node->right = pipeline_node;
-		if (pipeline_node->type == AST_ERR)
-			return (logic_node);
-		// logic_node->right = pipeline_node;
-		// append_child_node(logic_node, pipeline_node);
-		// append_child_node(logic_node, pipeline_node);
-		if (check(parser, TOKEN_AND) || check(parser, TOKEN_OR))
-		{
-			list_tail_node = parse_list_tail(parser, pipeline_node);
-			if (!list_tail_node) // err?
-				return (0);
-			append_child_node(pipeline_node, list_tail_node);
-		}
 	}
 	return (logic_node);
 }
@@ -924,97 +920,14 @@ t_node *parse_list(t_parser *parser, t_node *parent)
 		list_tail_node = parse_list_tail(parser, pipeline_node);
 		if (!list_tail_node) // err?
 			return (0);
-		// append_child_node(list_tail_node, pipeline_node);
 		list_tail_node->left = pipeline_node;
 		return (list_tail_node);
 	}
-	else if (check(parser, TOKEN_AND))
-	{
-		// list_tail_node = parse_list_tail(parser, pipeline_node);
-		// if (!list_tail_node) // err?
-		// 	return (0);
-		// append_child_node(list_tail_node, pipeline_node);
-		// list_tail_node->left = pipeline_node;
-		// return (list_tail_node);
-		// return (pipeline_node);
-	}
+	// else if (check(parser, TOKEN_AND))
+	// {
+	// }
 	return (pipeline_node);
 }
-// t_node *parse_list_tail(t_parser *parser, t_node *parent)
-// {
-// 	t_node			*logic_node;
-// 	t_node			*pipeline_node;
-// 	t_node			*list_tail_node;
-// 	(void)			parent;
-
-// 	logic_node = 0;
-// 	if (check(parser, TOKEN_AND) || check(parser, TOKEN_OR))
-// 	{
-// 		advance(parser);
-// 		pipeline_node = parse_pipeline(parser, parent);
-// 		if (!pipeline_node)
-// 		{
-// 			// free and handle err here?
-// 			return (0);
-// 		}
-// 		// logic_node->right = pipeline_node;
-// 		// logic_node->left = parent;
-// 		if (pipeline_node->type == AST_ERR)
-// 			return (pipeline_node);
-// 		// logic_node->right = pipeline_node;
-// 		// append_child_node(logic_node, pipeline_node);
-// 		if (check(parser, TOKEN_OR))
-// 		{
-// 			list_tail_node = parse_list_tail(parser, pipeline_node);
-// 			if (!list_tail_node) // err?
-// 				return (0);
-// 			append_child_node(pipeline_node, list_tail_node);
-// 		}
-// 		else if (check(parser, TOKEN_AND))
-// 		{
-// 			list_tail_node = parse_list_tail(parser, pipeline_node);
-// 			if (!list_tail_node) // err?
-// 				return (0);
-// 			list_tail_node->left = logic_node;
-// 			// append_child_node(list_tail_node, logic_node);
-// 			return (list_tail_node);
-// 		}
-// 	}
-// 	return (logic_node);
-// }
-
-// /**
-//  * @brief Parse function for <LIST>, calling <PIPELINE> and <LIST-TAIL>
-//  *
-//  * @param parser	parser struct
-//  * @return t_node*	root node of <LIST>
-//  */
-// t_node *parse_list(t_parser *parser, t_node *parent)
-// {
-// 	t_node		*pipeline_node;
-// 	t_node		*list_tail_node;
-// 	t_node		*logic_node;
-// 	t_node_type	state;
-
-// 	pipeline_node = parse_pipeline(parser, parent);
-// 	if (!pipeline_node) // err?
-// 		return (0);
-// 	if (check(parser, TOKEN_AND) || check(parser, TOKEN_OR))
-// 	{
-// 		state = AST_AND;
-// 		if (check(parser, TOKEN_OR))
-// 			state = AST_OR;
-// 		logic_node = create_node(state);
-// 		append_child_node(logic_node, pipeline_node);
-// 		list_tail_node = parse_list_tail(parser, logic_node);
-// 		if (!list_tail_node) // err?
-// 			return (0);
-// 		// append_child_node(list_tail_node, pipeline_node);
-// 		// list_tail_node->left = pipeline_node;
-// 		return (list_tail_node);
-// 	}
-// 	return (pipeline_node);
-// }
 
 /**
  * @brief Parse function for <SUBSHELL>, calling <LIST>
