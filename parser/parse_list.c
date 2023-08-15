30ea95d247a134d8e84e35364927efcede49f5d7
jusohn <jusohn@student.42seoul.kr>
2023-08-15 10:38:38 +0900

    [PARSE] REFACTOR: parse_list_tail 함수 간소화

--- parser/parse_list.c
+++ parser/parse_list.c
@@ -18,58 +18,35 @@ t_node *parse_list_tail(t_parser *parser, t_node *parent)
 	t_node			*logic_node;
 	t_node			*pipeline_node;
 	t_node			*list_tail_node;
-	t_node_type		state;
+	t_token_type	type;
 
-	logic_node = 0;
-	state = AST_AND;
-	if (check(parser, TOKEN_OR))
-		state = AST_OR;
-	logic_node = create_node(state);
+	logic_node = create_node(cur_type(parser) + ANDOR);
 	if (!logic_node)	// malloc err
 		return (0);
 	advance(parser);
 	pipeline_node = parse_pipeline(parser, logic_node);
-	if (!pipeline_node)
-	{
-		// free and handle err here?
-		free(logic_node);
-		return (0);
-	}
-	if (pipeline_node->type == AST_ERR)
+	if (!pipeline_node || pipeline_node->type == AST_ERR)
 	{
 		free_ast(logic_node);
 		return (pipeline_node);
 	}
-	if (check(parser, TOKEN_OR))
+	logic_node->left = parent;
+	type = cur_type(parser);
+	if (type == TOKEN_AND || type == TOKEN_OR)
 	{
 		list_tail_node = parse_list_tail(parser, pipeline_node);
 		if (!list_tail_node) // err?
 			return (0);
-		// append_child_node(logic_node, parent);
-		// append_child_node(logic_node, list_tail_node);
-		// append_child_node(list_tail_node, pipeline_node);
-		logic_node->left = parent;
 		logic_node->right = list_tail_node;
-		// list_tail_node->left = pipeline_node;
-	}
-	else if (check(parser, TOKEN_AND))
-	{
-		list_tail_node = parse_list_tail(parser, pipeline_node);
-		if (!list_tail_node) // err?
-			return (0);
-		// append_child_node(logic_node, parent);
-		// append_child_node(logic_node, pipeline_node);
-		// append_child_node(list_tail_node, pipeline_node);
-		logic_node->left = parent;
-		logic_node->right = pipeline_node;
-		list_tail_node->left = logic_node;
-		return (list_tail_node);
+		if (type == TOKEN_AND)
+		{
+			logic_node->right = pipeline_node;
+			list_tail_node->left = logic_node;
+			return (list_tail_node);
+		}
 	}
 	else
-	{
-		logic_node->left = parent;
 		logic_node->right = pipeline_node;
-	}
 	return (logic_node);
 }
 
