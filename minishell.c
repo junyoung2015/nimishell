3014176ab93cb454cfc0ea095e4f38172b5fb96d
jusohn <jusohn@student.42seoul.kr>
2023-08-28 18:46:40 +0900

    [PARSE] REFACTOR: main 간소화 및 모듈화 #57

--- minishell.c
+++ minishell.c
@@ -44,58 +44,6 @@ void	exit_err_with_msg(int code, char *file, char *msg, t_node *root)
 	exit(code);
 }
 
-#ifdef DEBUG
-
-// TODO: remove for
-void print_ast(t_node *node, int depth, const char *indent)
-{
-	if (node == NULL)
-	{
-		printf("%*s[%s]\n", depth * 4, "", indent);
-		return;
-	}
-	print_ast(node->right, depth + 1, "/");
-	printf("%*c", depth * 4, 1);
-	if (node->type == AST_PIPE)
-		printf("|");
-	else if (node->type == AST_NULL)
-		printf("NULL");
-	else if (node->type == AST_REDIR_IN)
-		printf("<");
-	else if (node->type == AST_REDIR_OUT)
-		printf(">");
-	else if (node->type == AST_REDIR_APPEND)
-		printf(">>");
-	else if (node->type == AST_AND)
-		printf("&&");
-	else if (node->type == AST_OR)
-		printf("||");
-	else if (node->type == AST_SUBSHELL)
-		printf("(SUB)");
-	else if (node->builtin != NOT_BUILTIN)
-		printf("[%d] ", node->builtin);
-	// printf("%*c", depth * 4, 32);
-	for (t_size i = 0; i < node->num_args; i++)
-		printf("[%s] ", node->cmd_args[i]);
-	printf("\n");
-	print_ast(node->left, depth + 1, "\\");
-}
-
-#endif
-
-#ifdef DEBUG
-
-void print_tokens(t_token *tokens, t_size num_tokens)
-{
-	printf("\n================= TOKENS =================\n");
-	printf("num of tokens: %llu\n", num_tokens);
-	for (t_size i = 0; i < num_tokens; i++)
-		printf("token[%llu]: [%d], [%s]\n", i, tokens[i].type, tokens[i].val);
-	printf("==========================================\n");
-}
-
-# endif
-
 static void	init_sh_info(int ac, char ** av, char **envp, t_sh_info *info)
 {
 	t_size	i;
@@ -110,18 +58,19 @@ static void	init_sh_info(int ac, char ** av, char **envp, t_sh_info *info)
 		cnt++;
 	info->env_cnt = cnt;
 	g_env = ft_calloc((cnt + 1), sizeof(char *));
-	// 널가드 추가
+	if (!g_env)
+		exit_err_with_msg(1, 0, MALLOC_ERR, 0);
 	i = 0;
 	while (i < cnt)
 	{
 		g_env[i] = ft_strdup(envp[i]);
-		// 널가드
+		if (!g_env[i])
+			exit_err_with_msg(1, 0, MALLOC_ERR, 0);
 		i++;
 	}
 	g_env[i] = NULL;
 }
 
-// Set terminal to not print ^C when Ctrl-C is pressed
 void	init_terminal(void)
 {
 	struct termios	term;
@@ -129,22 +78,20 @@ void	init_terminal(void)
 
 	status = tcgetattr(0, &term);
 	if (status == -1)
-	{
-		write(STD_ERR, "minishell: tcgetattr: ", 22);
-		write(STD_ERR, strerror(errno), ft_strlen(strerror(errno)));
-		write(STD_ERR, "\n", 1);
-		exit (1);
-	}
+		exit_err_with_msg(1, "tcgetattr", strerror(errno), 0);
 	term.c_lflag &= ~ECHOCTL;
 	status = tcsetattr(0, 0, &term);
 	if (status == -1)
-	{
-		write(STD_ERR, "minishell: tcgetattr: ", 22);
-		write(STD_ERR, strerror(errno), ft_strlen(strerror(errno)));
-		write(STD_ERR, "\n", 1);
-		exit (1);
-	}
+		exit_err_with_msg(1, "tcgetattr", strerror(errno), 0);
 	print_logo();
+}
+
+void	init_prompt(char **tokens, t_size *num_tokens, char **pwd)
+{
+	*tokens = 0;
+	*num_tokens = 0;
+	set_parent_signal();
+	*pwd = get_prompt();
 }
 
 int	main(int ac, char **av, char **envp)
@@ -159,10 +106,7 @@ int	main(int ac, char **av, char **envp)
 	init_terminal();
 	while (TRUE)
 	{
-		tokens = 0;
-		num_tokens = 0;
-		set_parent_signal();
-		pwd = get_prompt();
+		init_prompt(&line, &num_tokens, &pwd);
 		line = readline(pwd);
 		if (line)
 		{
@@ -184,15 +128,7 @@ int	main(int ac, char **av, char **envp)
 				}
 				else
 				{
-					if (DEBUG)
-						print_tokens(tokens, num_tokens);
 					info.ast = parse_tokens(tokens, num_tokens);
-					if (info.ast && DEBUG)
-					{
-						printf("\n=================== AST ==================\n");
-						print_ast(info.ast, 0, "");
-						printf("==========================================\n");
-					}
 					info.exit_code = executor(&info);
 				}
 			}
