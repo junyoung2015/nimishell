43647f7eb4ad20d4e1a2af9f8326314dca158e73
Jun Young Sohn <jusohn@student.42seoul.kr>
2023-08-28 20:32:26 +0900

    [MISC] REFACTOR: builtin 들을 norm 에 맞게 indent 및 header 추가

--- builtin/pwd.c
+++ builtin/pwd.c
@@ -1,16 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pwd.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jusohn <jusohn@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/28 20:31:43 by jusohn            #+#    #+#             */
+/*   Updated: 2023/08/28 20:31:55 by jusohn           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "builtin.h"
 
-void    pwd(t_exec_info *info)
+void	pwd(t_exec_info *info)
 {
-    char    *cwd;
+	char	*cwd;
 
-    cwd = getcwd(NULL, 0);
-    if (!cwd)
-    {
+	cwd = getcwd(NULL, 0);
+	if (!cwd)
+	{
 		err("minishell: pwd", info);
 		return ;
 	}
-    write(STDOUT_FILENO, cwd, ft_strlen(cwd));
-    write(STDOUT_FILENO, "\n", 1);
-    free(cwd);
+	write(STDOUT_FILENO, cwd, ft_strlen(cwd));
+	write(STDOUT_FILENO, "\n", 1);
+	free(cwd);
 }
