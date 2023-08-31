715cbfb2d99b9c30c52c99951e3aa96d22923b81
jusohn <jusohn@student.42seoul.kr>
2023-08-31 10:38:51 +0900

    [ENV] FEAT: SHLVL 을 위한 ft_atoi 추가

--- str/str_itoa.c
+++ str/str_itoa.c
@@ -60,3 +60,28 @@ char	*ft_itoa(int n)
 	put_num(result, n_long, digits - 1);
 	return (result);
 }
+
+int	ft_atoi(const char *str)
+{
+	int	result;
+	int	i;
+	int	sign;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (str[i] && (str[i] == ' ' || (9 <= str[i] && str[i] <= 13)))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -sign;
+		i++;
+	}
+	while (str[i] && ('0' <= str[i] && str[i] <= '9'))
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
