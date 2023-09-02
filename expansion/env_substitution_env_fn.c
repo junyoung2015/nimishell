937d8f8546eb8b2fdb90cd793ff544de9134b045
jusohn <jusohn@student.42seoul.kr>
2023-09-02 15:19:46 +0900

    [ENV] REFACTOR: env_str 와 process_env_dquote 간소화

--- expansion/env_substitution_env_fn.c
+++ expansion/env_substitution_env_fn.c
@@ -27,14 +27,10 @@ char	**env_str(char **in, t_exec_info *info)
 	while (**in && !is_dollar(**in))
 		(*in)++;
 	if (*in > start)
-	{
 		tmp = ft_substr(start, 0, *in - start);
-		if (!tmp)
-			return (0);
-	}
 	ft_arr_append_back(&result, tmp, 0);
 	prev = result;
-	if (!**in || !is_dollar(**in))
+	if (!**in || !is_dollar(**in) || !prev)
 		return (result);
 	result = handle_dollar_sign(in, result[0], 0, info);
 	free(prev[0]);
@@ -66,22 +62,45 @@ char	**env_squote(char **in, t_exec_info *info)
 	return (result);
 }
 
-t_bool	process_env_dquote(char **in, char ***res, char **s, t_exec_info *info)
+t_bool	append_or_concat_str(char *tmp, char **in, char ***res, t_exec_info *info)
 {
-	char	**dummy;
-	char	*tmp;
-	char	*temp;
-	char	*prev;
+	char	**arr;
 	t_size	len;
 
 	len = ft_arrlen(*res);
+	if (tmp && is_dollar(**in))
+	{
+		arr = *res;
+		*res = handle_dollar_sign(in, tmp, "\"", info);
+		free(tmp);
+		free(arr);
+		if (!*res)
+			return (FALSE);
+		len = ft_arrlen(*res);
+	}
+	else if (len > 0)
+		(*res)[len - 1] = tmp;
+	else
+	{
+		free((*res)[len]);
+		(*res)[len] = tmp;
+	}
+	return (TRUE);
+}
+
+t_bool	process_env_dquote(char **in, char ***res, char **s, t_exec_info *info)
+{
+	char	*tmp;
+	char	*temp;
+	char	*prev;
+
 	while (**in && !is_dquote(**in) && !is_dollar(**in))
 		(*in)++;
 	if (*in > *s || is_dquote(**in) || is_dollar(**in))
 	{
 		prev = 0;
-		if (len > 0)
-			prev = (*res)[len - 1];
+		if (ft_arrlen(*res) > 0)
+			prev = (*res)[ft_arrlen(*res) - 1];
 		tmp = ft_substr(*s, 0, *in - *s);
 		if (!tmp)
 			return (FALSE);
@@ -91,26 +110,8 @@ t_bool	process_env_dquote(char **in, char ***res, char **s, t_exec_info *info)
 		free(prev);
 		if (!tmp)
 			return (FALSE);
-		if (tmp && is_dollar(**in))
-		{
-			dummy = *res;
-			*res = handle_dollar_sign(in, tmp, "\"", info);
-			free(tmp);
-			free(dummy);	
-			if (!*res)
-				return (FALSE);
-			len = ft_arrlen(*res);
-		}
-		else
-		{
-			if (len > 0)
-				(*res)[len - 1] = tmp;
-			else
-			{
-				free((*res)[len]);
-				(*res)[len] = tmp;
-			}
-		}
+		if (!append_or_concat_str(tmp, in, res, info))
+			return (FALSE);
 	}
 	*s = *in;
 	return (TRUE);
